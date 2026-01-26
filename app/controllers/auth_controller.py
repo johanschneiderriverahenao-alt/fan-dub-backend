f2a18b478de9a8970b4751ef907622a59d2a2aec
johanschneiderriverahenao-alt <johanschneiderriverahenao@gmail.com>
2026-01-26 11:32:07 -0500

    error

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -250,6 +250,7 @@ class AuthController:
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not validate credentials"
             ) from e
+
     @staticmethod
     async def register(user_data: UserBase) -> JSONResponse:
         """
